18e24beeb93261f045367c128b938cfb27bbbf13
Juan Luna <juandluna17@gmail.com>
2023-06-08 16:45:36 -0500

    :seedling: First commit

---
+++ src/secondhandler.js
@@ -0,0 +1,23 @@
+"use strict";
+
+module.exports.handler = async (event) => {
+  const message = "Second Handler! Your function executed successfully!";
+
+  console.log(message);
+  console.log("event", event);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message,
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
